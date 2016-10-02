238db3ed81b6b89fb4de0a5af506d507dbc2424e
X O <xo@wvvw.me>
2016-10-02 18:12:03 +1100

    vue init

--- gui/slick/js/core.js
+++ gui/slick/js/core.js
@@ -74,6 +74,7 @@ $.ajax({
     MEDUSA.info = data;
     MEDUSA.info.themeSpinner = MEDUSA.info.themeName === 'dark' ? '-dark' : '';
     MEDUSA.info.loading = '<img src="images/loading16' + MEDUSA.info.themeSpinner + '.gif" height="16" width="16" />';
+    startVue(); // eslint-disable-line no-undef
 
     if (navigator.userAgent.indexOf('PhantomJS') === -1) {
         $(document).ready(UTIL.init);
